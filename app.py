6383ef27d5d1d718d239dbc7c7403510f4efaf98
Magdalena Czajka <magdalena.czajka94@gmail.com>
2020-05-05 15:02:06 +0200

    add new html templates for index and add pages

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+user = "David"
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Im working"
+    return render_template("index.html", user=user)
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
